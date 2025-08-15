9e628098d850005b841fb8e52c2a5228c1ad4a87
kezieroiwalla-lgtm <kezieroiwalla@gmail.com>
2025-08-15 14:23:57 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
+  SESSION_ID: "SUBZERO-MD;;;aUdRUbbR#pRFLKFvKY35-uLI3MXwM-owVCgAKCX50iQBGIH55Gs", // Put Session Id Here Inside "*"
   OWNER_NUMBER: "263719647303", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
